ca02dd0dcc9ffd918efeae0ed7f1ecc664ffdd0b
Fernando Silva <fdograph@gmail.com>
2020-11-27 18:27:12 +0100

    Updates

---
+++ precache-manifest.36562681d24e412ebb1fb92ad577e55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce303e54f58ce738561b469bd11deb51",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "5458587f89d8c5ce63b1",
+    "url": "/sudoku/static/css/main.8c8e60dc.chunk.css"
+  },
+  {
+    "revision": "25777bbff22021ec2ce8",
+    "url": "/sudoku/static/js/2.795af9f8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/sudoku/static/js/2.795af9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5458587f89d8c5ce63b1",
+    "url": "/sudoku/static/js/main.7b030c91.chunk.js"
+  },
+  {
+    "revision": "1e997d312c89287f084e",
+    "url": "/sudoku/static/js/runtime-main.75364a14.js"
+  }
+]);
